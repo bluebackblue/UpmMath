ef3e58177b5637085c6ff75b1b41168e558010cb
blueback <blueback28@gmail.com>
2022-03-08 19:27:49 +0900

    Quaternion

--- BlueBackMath/Assets/UPM/Runtime/BlueBack/Math/Quaternion.cs
+++ BlueBackMath/Assets/UPM/Runtime/BlueBack/Math/Quaternion.cs
@@ -38,9 +38,9 @@ namespace BlueBack.Math
 		*/
 		public static Unity.Mathematics.quaternion AxisAngle(in Unity.Mathematics.float3 a_axis,float a_radian)
 		{
-			float t_half_radian = a_radian * 0.5f;
-			float t_sin_half_theta = Unity.Mathematics.math.sin(t_half_radian);
-			float t_cos_half_theta = Unity.Mathematics.math.cos(t_half_radian);
+			float t_sin_half_theta;
+			float t_cos_half_theta;
+			Unity.Mathematics.math.sincos(a_radian * 0.5f,out t_sin_half_theta,out t_cos_half_theta);
 
 			return new Unity.Mathematics.quaternion(
 				a_axis.x * t_sin_half_theta,
@@ -143,6 +143,40 @@ namespace BlueBack.Math
 				float t_normalize = 0.25f / t_value;
 				return new Unity.Mathematics.quaternion((a_forward.x + t_right.z) * t_normalize,(t_up.z + a_forward.y) * t_normalize,t_value,(t_right.y - t_up.x) * t_normalize);
 			}
+		}
+
+		/** LookRotationMathematics
+
+			https://github.com/Unity-Technologies/Unity.Mathematics/blob/master/src/Unity.Mathematics/quaternion.cs#L390
+
+		*/
+		public static Unity.Mathematics.quaternion LookRotationMathematics(in Unity.Mathematics.float3 a_forward,in Unity.Mathematics.float3 a_up)
+		{
+            Unity.Mathematics.float3 t_right = Unity.Mathematics.math.normalize(Unity.Mathematics.math.cross(a_up,a_forward));
+            return CreateMathematics(in t_right,Unity.Mathematics.math.cross(a_forward,t_right),in a_forward);
+		}
+
+		/** CreateMathematics
+
+			https://github.com/Unity-Technologies/Unity.Mathematics/blob/master/src/Unity.Mathematics/quaternion.cs#L44
+
+		*/
+		public static Unity.Mathematics.quaternion CreateMathematics(in Unity.Mathematics.float3 a_right,in Unity.Mathematics.float3 a_up,in Unity.Mathematics.float3 a_forward)
+		{
+			uint t_right_sign = (Unity.Mathematics.math.asuint(a_right.x) & 0x80000000);
+			float t_value = a_up.y + Unity.Mathematics.math.asfloat(Unity.Mathematics.math.asuint(a_forward.z) ^ t_right_sign);
+			Unity.Mathematics.uint4 t_right_mask = Unity.Mathematics.math.uint4((int)t_right_sign >> 31);
+			Unity.Mathematics.uint4 t_value_mask = Unity.Mathematics.math.uint4(Unity.Mathematics.math.asint(t_value) >> 31);
+			Unity.Mathematics.uint4 t_sign_flips = Unity.Mathematics.math.uint4(0x00000000,0x80000000,0x80000000,0x80000000) ^ (t_right_mask & Unity.Mathematics.math.uint4(0x00000000,0x80000000,0x00000000,0x80000000)) ^ (t_value_mask & Unity.Mathematics.math.uint4(0x80000000,0x80000000,0x80000000,0x00000000));
+
+			Unity.Mathematics.quaternion t_quaternion;
+			{
+				t_quaternion.value = Unity.Mathematics.math.float4(1.0f + Unity.Mathematics.math.abs(a_right.x),a_right.y,a_forward.x,a_up.z) + Unity.Mathematics.math.asfloat(Unity.Mathematics.math.asuint(Unity.Mathematics.math.float4(t_value,a_up.x,a_right.z,a_forward.y)) ^ t_sign_flips);
+				t_quaternion.value = Unity.Mathematics.math.asfloat((Unity.Mathematics.math.asuint(t_quaternion.value) & ~t_right_mask) | (Unity.Mathematics.math.asuint(t_quaternion.value.zwxy) & t_right_mask));
+				t_quaternion.value = Unity.Mathematics.math.asfloat((Unity.Mathematics.math.asuint(t_quaternion.value.wzyx) & ~t_value_mask) | (Unity.Mathematics.math.asuint(t_quaternion.value) & t_value_mask));
+				t_quaternion.value = Unity.Mathematics.math.normalize(t_quaternion.value);
+			}
+			return t_quaternion;
 		}
 	}
 }

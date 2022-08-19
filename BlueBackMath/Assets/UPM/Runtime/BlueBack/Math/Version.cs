0cbff0c9a6cb85f82f45aded52b4aae400dfb444
blueback <blueback28@gmail.com>
2022-08-19 22:03:16 +0900

    整理

--- BlueBackMath/Assets/UPM/Runtime/BlueBack/Math/Version.cs
+++ BlueBackMath/Assets/UPM/Runtime/BlueBack/Math/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Math
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.5";
+		public const string packageversion = "0.0.6";
 
 		/** GetPackageVersion
 		*/
